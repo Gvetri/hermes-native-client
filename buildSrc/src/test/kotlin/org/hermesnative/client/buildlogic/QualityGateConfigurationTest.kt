be15f57689d08b215ac7e62c23e41777fb233250
Gvetri <8773754+Gvetri@users.noreply.github.com>
2026-09-07 13:42:49 +0000

    fix: correct KVM udev match rule

--- buildSrc/src/test/kotlin/org/hermesnative/client/buildlogic/QualityGateConfigurationTest.kt
+++ buildSrc/src/test/kotlin/org/hermesnative/client/buildlogic/QualityGateConfigurationTest.kt
@@ -33,7 +33,7 @@ class QualityGateConfigurationTest {
             listOf(
                 "      - name: Enable KVM group perms",
                 "        run: |",
-                "          echo 'KERNEL=\"kvm\", GROUP=\"kvm\", MODE=\"0666\", OPTIONS+=\"static_node=kvm\"' | sudo tee /etc/udev/rules.d/99-kvm4all.rules",
+                "          echo 'KERNEL==\"kvm\", GROUP=\"kvm\", MODE=\"0666\", OPTIONS+=\"static_node=kvm\"' | sudo tee /etc/udev/rules.d/99-kvm4all.rules",
                 "          sudo udevadm control --reload-rules",
                 "          sudo udevadm trigger --name-match=kvm",
             ).joinToString("\n")
